9b5c5246e6ef3666edae87002171f4b0a6b4a9c0
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-06-01 20:30:08 +0000

    Grocery

---
+++ Gro.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Gro{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        while (t-- > 0) {
+            int n = scanner.nextInt();
+            int x = scanner.nextInt();
+            int[] a = new int[n];
+            int[] b = new int[n];
+            
+            for (int i = 0; i < n; i++) {
+                a[i] = scanner.nextInt();
+            }
+            
+            for (int j= 0; j< n; j++) {
+                b[j] = scanner.nextInt();
+            }
+            
+            // Your code goes here
+            int sum=0;
+            for(int i =0;  i< n; i++){
+                
+                
+                if(a[i] >= x){
+                    sum= sum + b[i];
+                    
+                    
+                    
+                }
+            }
+            System.out.println(sum);
+        }
+    }
+}
