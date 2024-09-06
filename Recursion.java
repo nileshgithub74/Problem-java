d7f642e80b89f786ef1fdb1fa603fe04b99f9a78
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-09-06 12:25:33 +0000

    java

--- Recursion.java
+++ Recursion.java
@@ -1,20 +1,27 @@
-public class Recursion {
+// public class Recursion {
 
 
-    public static int Sum(int n ){
-        if(n <= 0){
-            return  0;
-        }
-        else{
-            return n + Sum(n-1);
-        }
-    }
-    public static void main(String[] args){
+//     public static int Sum(int n ){
+//         if(n <= 0){
+//             return  0;
+//         }
+//         else{
+//             return n + Sum(n-1);
+//         }
+//     }
+//     public static void main(String[] args){
 
-        int num = 10;
+//         int num = 10;
          
-        System.out.println(Sum(num));
+//         System.out.println(Sum(num));
 
-    }
+//     }
     
+// }
+
+public class Recursion {
+    public static void main(Strings[] args){
+        
+    }
 }
+
