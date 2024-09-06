810c8ec6c11626be855ed5a49f2d7323512a9fd1
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-09-06 12:16:53 +0000

    java

---
+++ ArrayDemo.java
@@ -0,0 +1,36 @@
+// import java.util.Scanner;
+
+// public class ArrayDemo {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("Enter the rows: ");
+//         int row = sc.nextInt();
+
+//         System.out.println("Enter the columns: ");
+//         int columns = sc.nextInt();
+
+//         int[][] arr = new int[row][columns];
+
+//         System.out.println("Enter the elements in the array: ");
+
+//         for (int i = 0; i < row; i++) {
+//             for (int j = 0; j < columns; j++) {
+//                 arr[i][j] = sc.nextInt();
+//             }
+//         }
+
+//         System.out.println("Array is: ");
+
+//         for (int i = 0; i < row; i++) {
+//             for (int j = 0; j < columns; j++) {
+//                 System.out.print(arr[i][j] + " ");
+//             }
+//             System.out.println();
+//         }
+
+//         sc.close();
+//     }
+// }
+
+
